a7edb6e005f6a8fa696f04dfe6405fc9148fe607
Sascha Greuel <github@1-2.dev>
2025-11-09 14:50:32 +0100

    Initial Commit

---
+++ generateConstants.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!\file_exists('option.xml')) {
+    echo "option.xml not found.";
+
+    exit(0);
+}
+
+$xmlString = \file_get_contents('option.xml');
+
+try {
+    $xml = new SimpleXMLElement($xmlString);
+} catch (Exception $e) {
+    exit(0);
+}
+
+$namespaces = $xml->getNamespaces(true);
+$xml->registerXPathNamespace('ns', $namespaces['']);
+
+$constants = ["const WCF_N = 1;"];
+
+foreach ($xml->xpath('//ns:import/ns:options/ns:option') as $option) {
+    $name = \strtoupper(\str_replace(['.', ':'], '_', (string)$option['name']));
+    $defaultValue = (string)$option->defaultvalue;
+    $optionType = (string)$option->optiontype;
+
+    if ($defaultValue === '') {
+        $constants[] = "const {$name} = '';";
+    } elseif ($optionType === 'boolean' || $optionType === 'integer') {
+        $constants[] = "const {$name} = " . (int)$defaultValue . ";";
+    } else {
+        $constants[] = "const {$name} = '{$defaultValue}';";
+    }
+}
+
+\file_put_contents('constants.php', "<?php\n\n" . \implode("\n", $constants) . "\n");
+
+echo "constants.php has been generated successfully.";
---
+++ pkgbase-62.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+rm -rf pkgbase
+git clone https://github.com/SoftCreatRMedia/pkgbase-61.git pkgbase
+rm -rf pkgbase/.git
+rm -rf .github
+rm -f .gitignore
+rm -f .eslintignore
+rm -f .eslintrc.js
+rm -f eslint.config.mjs
+rm -f .php-cs-fixer.dist.php
+rm -f .phpcs.xml
+rm -f .prettierrc
+rm -f package-lock.json
+rm -f package.json
+rm -f tsconfig.json
+cp -a pkgbase/. .
+rm -rf pkgbase

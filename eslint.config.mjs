0acb4938c77929c0372fce95d54b6665e69a8eb6
Sascha Greuel <github@1-2.dev>
2026-09-02 10:10:48 +0200

    Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,61 +8,61 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-	{
-		ignores: ["**/*.js", "**/extra", "node_modules/**/*", "eslint.config.mjs"],
-	},
-	...compat.extends(
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-		"prettier",
-	),
-	{
-		plugins: {
-			"@typescript-eslint": typescriptEslint,
-		},
+  {
+    ignores: ["**/*.js", "**/extra", "node_modules/**/*", "eslint.config.mjs"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
 
-		languageOptions: {
-			parser: tsParser,
-			ecmaVersion: 5,
-			sourceType: "script",
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
 
-			parserOptions: {
-				tsconfigRootDir: __dirname,
-				project: ["./tsconfig.json"],
-			},
-		},
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
 
-		rules: {
-			"@typescript-eslint/no-explicit-any": 0,
-			"@typescript-eslint/no-non-null-assertion": 0,
-			"@typescript-eslint/no-unsafe-argument": 0,
-			"@typescript-eslint/no-unsafe-assignment": 0,
-			"@typescript-eslint/no-unsafe-call": 0,
-			"@typescript-eslint/no-unsafe-member-access": 0,
-			"@typescript-eslint/no-unsafe-return": 0,
+    rules: {
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/no-non-null-assertion": 0,
+      "@typescript-eslint/no-unsafe-argument": 0,
+      "@typescript-eslint/no-unsafe-assignment": 0,
+      "@typescript-eslint/no-unsafe-call": 0,
+      "@typescript-eslint/no-unsafe-member-access": 0,
+      "@typescript-eslint/no-unsafe-return": 0,
 
-			"@typescript-eslint/no-unused-vars": [
-				"error",
-				{
-					argsIgnorePattern: "^_",
-					varsIgnorePattern: "^_",
-				},
-			],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
 
-			"@typescript-eslint/no-misused-promises": [
-				"error",
-				{
-					checksVoidReturn: false,
-				},
-			],
-			"@typescript-eslint/prefer-promise-reject-errors": ["error", { allowEmptyReject: true }],
-		},
-	},
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: false,
+        },
+      ],
+      "@typescript-eslint/prefer-promise-reject-errors": ["error", { allowEmptyReject: true }],
+    },
+  },
 ];
